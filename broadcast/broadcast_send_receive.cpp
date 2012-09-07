2fb5230c9d44261b068760d809b3673863c8d259
Carlos Raoni Alencar Mendes <carlosraoni@gmail.com>
2012-09-06 22:26:00 -0300

    Versão do broadcast considerando que cada thread ou recebe ou envia.

--- broadcast/broadcast_send_receive.cpp
+++ broadcast/broadcast_send_receive.cpp
@@ -12,8 +12,9 @@
 
 using namespace std;
 
-const int BUFFER_SIZE = 10;
-const int NUM_TOTAL_THREADS = 10;
+const int BUFFER_SIZE = 5;
+const int NSENDERS = 10;
+const int NRECEIVERS = 10;
 
 typedef enum {FREE, USED} BufferEntryStatus;
 
@@ -22,25 +23,28 @@ vector<BufferEntryStatus> buffer_status(BUFFER_SIZE, FREE);
 int total_free_entrys = BUFFER_SIZE;
 
 int last_send_message = 0;
-vector<int> last_read(NUM_TOTAL_THREADS, 0);
+vector<int> last_read(NRECEIVERS, 0);
 map<int, int> map_msg_buffer_index;
 
 int delayed_senders = 0;
-vector<int> delayed_receive(NUM_TOTAL_THREADS, 0);
+vector<int> delayed_receive(NRECEIVERS, 0);
 
 sem_t e; // semaforo da exclusao mutua
-sem_t sem_receive[NUM_TOTAL_THREADS];
+sem_t sem_receive[NRECEIVERS];
 sem_t sem_send;
 
-void send(int id){
+void send_message(int id, const string & msg){
 
     sem_wait(&e);
+    cout << "Thread " << id << " tentando enviar mensagem " << msg  << ", tfe = " << total_free_entrys << endl;
     if(total_free_entrys == 0){
         delayed_senders++;
         sem_post(&e);
         sem_wait(&sem_send);
     }
+
     total_free_entrys--;
+
     // Procura uma entrada livre no buffer
     int writing_index = -1;
     for(int i=0; i<BUFFER_SIZE; i++){
@@ -54,16 +58,14 @@ void send(int id){
     }
 
     //EscreveDados(proximoaserescrito, dados);
-    ostringstream msg;
-    msg << "[" << id << ", " << writing_index << "]";
-    cout << "Thread " << id << " wrote the message: " << msg.str() << endl;
-    buffer[writing_index] = msg.str();
+    cout << "buffer[" << writing_index << "] = " << msg << endl;
+    buffer[writing_index] = msg;
     map_msg_buffer_index[++last_send_message] = writing_index;
     buffer_status[writing_index] = USED;
 
     //SIGNAL
     int delayed_receiver = -1;
-    for(int i=0; i<NUM_TOTAL_THREADS; i++){
+    for(int i=0; i<NRECEIVERS; i++){
         if(delayed_receive[i] > 0){
             delayed_receiver = i;
             break;
@@ -79,23 +81,27 @@ void send(int id){
 //>
 }
 
-void receive(int id, vector<string> & received){
+void receive_message(int id, vector<string> & received){
 
     sem_wait(&e);
     if(last_read[id] == last_send_message){
         delayed_receive[id]++;
         sem_post(&e);
         sem_wait(&sem_receive[id]);
+
     }
+    // SIGNAL
     sem_post(&e);
+
 
     int next_to_read = last_read[id] + 1;
     received.push_back(buffer[map_msg_buffer_index[next_to_read]]);
-    last_read[id] = next_to_read;
 
     sem_wait(&e);
+    last_read[id] = next_to_read;
+
     bool message_has_pending_reads = false;
-    for(int i=0; i<NUM_TOTAL_THREADS; i++){
+    for(int i=0; i<NRECEIVERS; i++){
         if(last_read[i] < next_to_read){
             message_has_pending_reads = true;
             break;
@@ -108,37 +114,49 @@ void receive(int id, vector<string> & received){
     }
 
     // SIGNAL
-    int delayed_receiver = -1;
-    for(int i=0; i<NUM_TOTAL_THREADS; i++){
-        if(delayed_receive[i] > 0 && last_read[i] < last_send_message){
-            delayed_receiver = i;
-            break;
-        }
-    }
-    if(delayed_receiver >= 0){
-        delayed_receive[delayed_receiver]--;
-        sem_post(&sem_receive[delayed_receiver]);
-    }
-    else if(delayed_senders > 0 && total_free_entrys > 0){
+    if(delayed_senders > 0 && total_free_entrys > 0){
         delayed_senders--;
         sem_post(&sem_send);
     }
     else{
-        sem_post(&e);
+        int delayed_receiver = -1;
+        for(int i=0; i<NRECEIVERS; i++){
+            if(delayed_receive[i] > 0 && last_read[i] < last_send_message){
+                delayed_receiver = i;
+                break;
+            }
+        }
+        if(delayed_receiver >= 0){
+            delayed_receive[delayed_receiver]--;
+            sem_post(&sem_receive[delayed_receiver]);
+        }
+        else{
+            sem_post(&e);
+        }
     }
 }
 
-void * action(void * arg){
+void * sender_main(void * arg){
 	int threadId = *((int *) arg);
 	vector<string> received;
 
-    send(threadId);
-    while(received.size() < NUM_TOTAL_THREADS){
-        receive(threadId, received);
+    for(int i=0; i<10; i++){
+        ostringstream out;
+        out << "[" << threadId << ","<< i << "]";
+        send_message(threadId, out.str());
+    }
+}
+
+void * receiver_main(void * arg){
+	int threadId = *((int *) arg);
+	vector<string> received;
+
+    while(last_read[threadId] < 100){
+        receive_message(threadId, received);
     }
 
     sem_wait(&e);
-    cout << "Rec. Msgs from thread " << threadId << ": ";;
+    cout << "Rec. Msgs from thread " << threadId << ": ";
     for(int i=0; i<received.size(); i++){
         cout << received[i];
     }
@@ -148,22 +166,33 @@ void * action(void * arg){
 
 int main(int argc, char ** argv){
 
-	pthread_t thread[NUM_TOTAL_THREADS];
+	pthread_t senders[NSENDERS];
+	pthread_t receivers[NRECEIVERS];
 
 	sem_init(&e, 0, 1);
 	sem_init(&sem_send, 0, 0);
-	for(int i=0; i<NUM_TOTAL_THREADS; i++){
+	for(int i=0; i<NRECEIVERS; i++){
         sem_init(&sem_receive[i], 0, 0);
 	}
 
-	for(int i=0; i<NUM_TOTAL_THREADS; i++){
-		if (pthread_create(&thread[i], NULL, action, (void *) new int(i))){
-			cout << "Error creating thread " << i << "!" << endl;
+	for(int i=0; i<NRECEIVERS; i++){
+		if (pthread_create(&receivers[i], NULL, receiver_main, (void *) new int(i))){
+			cout << "Error creating thread receiver " << i << "!" << endl;
 		}
 	}
 
-	for (int i = 0; i < NUM_TOTAL_THREADS; i++){
-		pthread_join(thread[i], NULL);
+	for(int i=0; i<NSENDERS; i++){
+		if (pthread_create(&senders[i], NULL, sender_main, (void *) new int(i))){
+			cout << "Error creating thread sender" << i << "!" << endl;
+		}
+	}
+
+	for (int i = 0; i < NRECEIVERS; i++){
+		pthread_join(receivers[i], NULL);
+	}
+
+	for (int i = 0; i < NSENDERS; i++){
+		pthread_join(senders[i], NULL);
 	}
 
 	return 0;
