21a0959831581bb433d32feda5338524273ca7c9
Carlos Raoni Alencar Mendes <carlosraoni@gmail.com>
2012-09-12 11:02:56 -0300

    Implementação considerando threads recebendo e enviando e início de comentários para entrega do trabalho

--- broadcast/broadcast_send_receive.cpp
+++ broadcast/broadcast_send_receive.cpp
@@ -1,3 +1,62 @@
+/*
+
+Trabalho da disciplina INF2591 - Programação concorrente
+Prof.: Noemi
+
+Aluno: Carlos Raoni de Alencar Mendes
+
+Broadcast via buffer circular - Solução usando notação await:
+
+// Estrutura de dados
+int BUFFER_SIZE; // Tamanho do buffer
+int NTHREADS; // Número total de threads
+
+Dados buffer[BUFFER_SIZE]; // Buffer para armazenamento das mensagens transmitidas
+BufferStatus buffer_status[BUFFER_SIZE]; // Controla o estado (livre ou usado) de cada posição do buffer
+
+int last_send_message; // Sequencial que indica a última mensagem que foi enviada
+int next_to_write; // Indica a próxima posição do buffer que deve ser usada para escrita
+
+int last_read[NTHREADS]; // Armazena para cada thread qual foi a última mensagem que a mesma leu
+
+send(int threadId, Dados msg){
+    <
+        await(buffer_status[next_to_write] == FREE) // Aguarda a posição do buffer para escrita estar livre
+
+        buffer[next_to_write] = msg; // Escreve mensagem no buffer
+        buffer_status[next_to_write] = USED; // Marca o status da posição de escrita para usado
+        last_send_message++; // Atualiza o identificador da última mensagem transmitida
+        next_to_write = (next_to_write + 1) % BUFFER_SIZE; // Atualiza a posição para escrita
+    >
+}
+
+receive(int threadId){
+    <
+        await(last_read[threadId] < last_send_message) // Aguarda até que existam mensagens que não foram lidas
+    >
+
+    int next_to_read = (last_read[id] + 1) % BUFFER_SIZE; // Determina a posição do buffer da próxima mensagem a ser lida
+    LeDados(buffer[next_to_read]); // Lê a mensagem do buffer
+
+    <
+        last_read[threadId]++; // Atualiza última mensagem lida pela thread
+        int read_message = last_read[threadId]; // Armazena identificador da mensagem que acabou de ser lida pela thread
+
+        bool message_has_pending_reads = false; // Indica se a mensagem que acabou de ser lida possui threads que ainda não a leram
+        for(int i=0; i<NTHREADS; i++){
+            if(last_read[i] < read_message){ // Verfica se a thread de idenficador id ainda não leu a mensagem
+                message_has_pending_reads = true;
+                break;
+            }
+        }
+        if(!message_has_pending_reads){ // Verifica se a mensagem que acabou de ser lida já foi lida por todas as threads
+            buffer_status[next_to_read] = FREE; // Libera a posição do buffer que a mensagem já lida por todos ocupava
+        }
+    >
+}
+
+*/
+
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
@@ -12,61 +71,96 @@
 
 using namespace std;
 
-const int BUFFER_SIZE = 5;
-const int NSENDERS = 10;
-const int NRECEIVERS = 10;
+const int BUFFER_SIZE = 5; // Tamanho do buffer
+const int NTHREADS = 10; // Número total de threads
 
-typedef enum {FREE, USED} BufferEntryStatus;
+typedef enum {FREE, USED} BufferEntryStatus; // Enum de status de cada entrada do buffer
 
-vector<string> buffer(BUFFER_SIZE);
-vector<BufferEntryStatus> buffer_status(BUFFER_SIZE, FREE);
-int total_free_entrys = BUFFER_SIZE;
+vector<string> buffer(BUFFER_SIZE); // Buffer para armazenamento das mensagens transmitidas
+vector<BufferEntryStatus> buffer_status(BUFFER_SIZE, FREE); // Controla o estado (livre ou usado) de cada posição do buffer, sendo o valor inicial livre para todas as entradas
 
-int last_send_message = 0;
-vector<int> last_read(NRECEIVERS, 0);
-map<int, int> map_msg_buffer_index;
+int last_send_message = -1; // Sequencial que indica a última mensagem que foi enviada
+int next_to_write = 0; // Indica a próxima posição do buffer que deve ser usada para escrita
+vector<int> last_read(NTHREADS, -1); // Armazena para cada thread qual foi a última mensagem que a mesma leu
 
-int delayed_senders = 0;
-vector<int> delayed_receive(NRECEIVERS, 0);
+// Variáveis utilizadas para implementação da passagem de bastão
+int delayed_senders = 0; // Contador do número de threads suspensas ao tentar enviar mensagem
+vector<int> delayed_receive(NTHREADS, 0); // Indicador para cada thread se a mesma está suspensa esperando a próxima mensagem para leitura
 
-sem_t e; // semaforo da exclusao mutua
-sem_t sem_receive[NRECEIVERS];
-sem_t sem_send;
+sem_t e; // Semáforo da exclusao mutua
+sem_t sem_receive[NTHREADS]; // Semáforo de leitura para cada thread
+sem_t sem_send; // Semáforo de escrita
 
-void send_message(int id, const string & msg){
+// Primitiva send recebe o identificador da thread e a mensagem a ser enviada
+void send(int id, const string & msg){
 
-    sem_wait(&e);
-    cout << "Thread " << id << " tentando enviar mensagem " << msg  << ", tfe = " << total_free_entrys << endl;
-    if(total_free_entrys == 0){
+    // < await(buffer_status[next_to_write] == FREE) // Aguarda a posição do buffer para escrita estar livre
+    sem_wait(&e); // P(e)
+    if(buffer_status[next_to_write] == USED){
         delayed_senders++;
-        sem_post(&e);
-        sem_wait(&sem_send);
+        sem_post(&e); // V(e)
+        sem_wait(&sem_send); // P(sem_send)
     }
 
-    total_free_entrys--;
+    cout << "buffer[" << next_to_write << "] = " << msg << endl;
 
-    // Procura uma entrada livre no buffer
-    int writing_index = -1;
-    for(int i=0; i<BUFFER_SIZE; i++){
-        if(buffer_status[i] == FREE){
-            writing_index = i;
+    // Escrevendo no buffer e atualizando estruturas
+    buffer[next_to_write] = msg; // Escreve mensagem no buffer
+    buffer_status[next_to_write] = USED; // Marca o status da posição de escrita para usado
+    last_send_message++; // Atualiza o identificador da última mensagem transmitida
+    next_to_write = (next_to_write + 1) % BUFFER_SIZE; // Atualiza a posição para escrita
+
+    //SIGNAL
+    int delayed_receiver = -1; // Identificador de thread que estivesse suspensa aguardando nova mensagem a ser lida
+    for(int i=0; i<NTHREADS; i++){
+        if(delayed_receive[i] > 0){ // Verifica se a thread de id i está suspensa aguardando mensagem
+            delayed_receiver = i;
             break;
         }
     }
-    if(writing_index == -1){
-        cout << "ERRO: Nao encontrou posicao livre no buffer!" << endl;
+    if(delayed_receiver >= 0){ // Se existia alguma thread suspensa esperando mensagem
+        delayed_receive[delayed_receiver]--; // Decrementa contador de thread suspensa por leitura
+        sem_post(&sem_receive[delayed_receiver]); // V(sem_receive[delayed_receiver])
+    }
+    else{
+        sem_post(&e); // V(e)
+    }
+    //>
+}
+
+// Primitiva receive recebe o identificador da thread e um vetor dinâmico que armazena as mensagens recebidas sequencialmente
+void receive(int id, vector<string> & received){
+
+    sem_wait(&e);
+    if(last_read[id] == last_send_message){
+        delayed_receive[id]++;
+        sem_post(&e);
+        sem_wait(&sem_receive[id]);
+    }
+    // SIGNAL
+    sem_post(&e);
+
+    int next_to_read = (last_read[id] + 1) % BUFFER_SIZE;
+    received.push_back(buffer[next_to_read]);
+
+    sem_wait(&e);
+    int read_message = ++last_read[id];
+
+    bool message_has_pending_reads = false;
+    for(int i=0; i<NTHREADS; i++){
+        if(last_read[i] < read_message){
+            message_has_pending_reads = true;
+            break;
+        }
+    }
+    if(!message_has_pending_reads){
+        buffer_status[next_to_read] = FREE;
     }
 
-    //EscreveDados(proximoaserescrito, dados);
-    cout << "buffer[" << writing_index << "] = " << msg << endl;
-    buffer[writing_index] = msg;
-    map_msg_buffer_index[++last_send_message] = writing_index;
-    buffer_status[writing_index] = USED;
-
-    //SIGNAL
+    // SIGNAL
     int delayed_receiver = -1;
-    for(int i=0; i<NRECEIVERS; i++){
-        if(delayed_receive[i] > 0){
+    for(int i=0; i<NTHREADS; i++){
+        if(delayed_receive[i] > 0 && last_read[i] < last_send_message){
             delayed_receiver = i;
             break;
         }
@@ -75,84 +169,25 @@ void send_message(int id, const string & msg){
         delayed_receive[delayed_receiver]--;
         sem_post(&sem_receive[delayed_receiver]);
     }
-    else{
-        sem_post(&e);
-    }
-//>
-}
-
-void receive_message(int id, vector<string> & received){
-
-    sem_wait(&e);
-    if(last_read[id] == last_send_message){
-        delayed_receive[id]++;
-        sem_post(&e);
-        sem_wait(&sem_receive[id]);
-
-    }
-    // SIGNAL
-    sem_post(&e);
-
-
-    int next_to_read = last_read[id] + 1;
-    received.push_back(buffer[map_msg_buffer_index[next_to_read]]);
-
-    sem_wait(&e);
-    last_read[id] = next_to_read;
-
-    bool message_has_pending_reads = false;
-    for(int i=0; i<NRECEIVERS; i++){
-        if(last_read[i] < next_to_read){
-            message_has_pending_reads = true;
-            break;
-        }
-    }
-    if(!message_has_pending_reads){
-        buffer_status[map_msg_buffer_index[next_to_read]] = FREE;
-        map_msg_buffer_index.erase(next_to_read);
-        total_free_entrys++;
-    }
-
-    // SIGNAL
-    if(delayed_senders > 0 && total_free_entrys > 0){
+    else if(delayed_senders > 0 && buffer_status[next_to_read] == FREE){
         delayed_senders--;
         sem_post(&sem_send);
     }
     else{
-        int delayed_receiver = -1;
-        for(int i=0; i<NRECEIVERS; i++){
-            if(delayed_receive[i] > 0 && last_read[i] < last_send_message){
-                delayed_receiver = i;
-                break;
-            }
-        }
-        if(delayed_receiver >= 0){
-            delayed_receive[delayed_receiver]--;
-            sem_post(&sem_receive[delayed_receiver]);
-        }
-        else{
-            sem_post(&e);
-        }
+        sem_post(&e);
     }
 }
 
-void * sender_main(void * arg){
+void * thread_main(void * arg){
 	int threadId = *((int *) arg);
 	vector<string> received;
 
-    for(int i=0; i<10; i++){
-        ostringstream out;
-        out << "[" << threadId << ","<< i << "]";
-        send_message(threadId, out.str());
-    }
-}
+    ostringstream out;
+    out << "[" << threadId << "]";
+    send(threadId, out.str());
 
-void * receiver_main(void * arg){
-	int threadId = *((int *) arg);
-	vector<string> received;
-
-    while(last_read[threadId] < 100){
-        receive_message(threadId, received);
+    while(last_read[threadId] < NTHREADS){
+        receive(threadId, received);
     }
 
     sem_wait(&e);
@@ -166,33 +201,22 @@ void * receiver_main(void * arg){
 
 int main(int argc, char ** argv){
 
-	pthread_t senders[NSENDERS];
-	pthread_t receivers[NRECEIVERS];
+	pthread_t t[NTHREADS];
 
 	sem_init(&e, 0, 1);
 	sem_init(&sem_send, 0, 0);
-	for(int i=0; i<NRECEIVERS; i++){
+	for(int i=0; i<NTHREADS; i++){
         sem_init(&sem_receive[i], 0, 0);
 	}
 
-	for(int i=0; i<NRECEIVERS; i++){
-		if (pthread_create(&receivers[i], NULL, receiver_main, (void *) new int(i))){
-			cout << "Error creating thread receiver " << i << "!" << endl;
+	for(int i=0; i<NTHREADS; i++){
+		if (pthread_create(&t[i], NULL, thread_main, (void *) new int(i))){
+			cout << "Error creating thread " << i << "!" << endl;
 		}
 	}
 
-	for(int i=0; i<NSENDERS; i++){
-		if (pthread_create(&senders[i], NULL, sender_main, (void *) new int(i))){
-			cout << "Error creating thread sender" << i << "!" << endl;
-		}
-	}
-
-	for (int i = 0; i < NRECEIVERS; i++){
-		pthread_join(receivers[i], NULL);
-	}
-
-	for (int i = 0; i < NSENDERS; i++){
-		pthread_join(senders[i], NULL);
+	for (int i = 0; i < NTHREADS; i++){
+		pthread_join(t[i], NULL);
 	}
 
 	return 0;
